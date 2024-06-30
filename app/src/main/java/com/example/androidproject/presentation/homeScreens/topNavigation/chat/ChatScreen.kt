d0af49f19b83176ac0db4d608dd0104d3a390b92
dina othman <dinaosman581@gmail.com>
2024-06-30 13:08:05 +0300

    add ChatScreen

---
+++ app/src/main/java/com/example/androidproject/presentation/homeScreens/topNavigation/chat/ChatScreen.kt
@@ -0,0 +1,32 @@
+package com.example.androidproject.presentation.homeScreens.topNavigation.chat
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ChatScreen(){
+    Box(modifier = Modifier.fillMaxSize()){
+        Column(modifier=Modifier.fillMaxSize().align(Alignment.Center),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text="Chat", fontSize = 18.sp, style = TextStyle(color = Color.Blue))
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ChatPreview() {
+    ChatScreen()
+}
